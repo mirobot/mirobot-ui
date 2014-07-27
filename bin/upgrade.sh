b74ae8109a7d203178e0d659543baa7a6d6e116b
Ben Pirt <ben@pirt.co.uk>
2014-07-27 09:59:21 +0100

    Allow upgrade script to be used on other IPs

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --request POST http://mirobot:@10.10.100.254/data_success.html -F "CMD=WEB_UPLOAD" -F "files=@dist/mirobot.bin"
+curl --request POST http://mirobot:@${1:-10.10.100.254}/data_success.html -F "CMD=WEB_UPLOAD" -F "files=@dist/mirobot.bin"
